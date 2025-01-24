70dea0b4a2f8517c478cfcb39ae19cde72bba602
samuel do <DDDD06.23@ichat.sp.edu.sg>
2025-01-24 19:13:44 +0800

    commit

--- CSAD/csad-project/src/header.jsx
+++ CSAD/csad-project/src/header.jsx
@@ -5,30 +5,16 @@ const Header = () => (
         <div className="form-control">
             <input type="value" required />
             <label>
-                <span style={{ transitionDelay: '0ms' }}>U</span>
-                <span style={{ transitionDelay: '50ms' }}>s</span>
-                <span style={{ transitionDelay: '100ms' }}>e</span>
-                <span style={{ transitionDelay: '150ms' }}>r</span>
-                <span style={{ transitionDelay: '200ms' }}>n</span>
-                <span style={{ transitionDelay: '250ms' }}>a</span>
-                <span style={{ transitionDelay: '300ms' }}>m</span>
-                <span style={{ transitionDelay: '350ms' }}>e</span>
+                <span style={{ transitionDelay: '0ms' }}>Username</span>
             </label>
         </div>
         <div className="form-control">
             <input type="value" required />
             <label>
-                <span style={{ transitionDelay: '0ms' }}>P</span>
-                <span style={{ transitionDelay: '50ms' }}>a</span>
-                <span style={{ transitionDelay: '100ms' }}>s</span>
-                <span style={{ transitionDelay: '150ms' }}>s</span>
-                <span style={{ transitionDelay: '200ms' }}>w</span>
-                <span style={{ transitionDelay: '250ms' }}>o</span>
-                <span style={{ transitionDelay: '300ms' }}>r</span>
-                <span style={{ transitionDelay: '350ms' }}>d</span>
+                <span style={{ transitionDelay: '0ms' }}>Password</span>
             </label>
         </div>
-        <button className="btn">Submit</button>
+        <button className="btn">Log In</button>
     </form>
 );
 export default Header
